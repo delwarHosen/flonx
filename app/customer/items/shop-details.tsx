1bd8ce6b651a42d1c7b7e314e6042e256b1a1193
delwarHosen <delwar.hosen008@gmail.com>
2026-03-18 11:51:50 +0600

    some issue solve

--- app/customer/items/shop-details.tsx
+++ app/customer/items/shop-details.tsx
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: wp(20),
     },
     logoWrapper: {
-        width: wp(100),
+        width: 100,
         height: 100,
         borderRadius: 20,
         backgroundColor: '#FFFFFF',
